c7e818e14c7cad0afc336cff86e3cd2f69640859
malleswari <maddikirimalleswari4@gmail.com>
2025-11-11 22:24:41 +0530

    terraform

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,14 @@
 output "vpc_id" {
   value = aws_vpc.main.id
 }
+
+ output "public_subnet_ids" {
+  value = aws_subnet.public[*].id  # * means all ids give
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id  # * means all ids give
+}
+output "database_subnet_ids" {
+  value = aws_subnet.database[*].id  # * means all ids give
+}
